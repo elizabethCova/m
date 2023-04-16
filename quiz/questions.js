f8c4087fc18fc08073761d980cd19199f9c54969
elizabethCova <129977592+elizabethCova@users.noreply.github.com>
2023-04-16 17:28:31 -0600

    Add files via upload

---
+++ quiz/questions.js
@@ -0,0 +1,70 @@
+let questions = [
+    {
+    numb: 1,
+    question: "¿Qué necesitamos para ver?",
+    answer: "Una onda electromagnética",
+    options: [
+      "Un campo electromagnético",
+      "Una onda electromagnética",
+      "Un campo gravitacional",
+      "Dos polos opuestos"
+    ]
+  },
+    {
+    numb: 2,
+    question: "En física no estamos seguros de a donde va o en donde está una partícula, ¿A que principio se refiere?",
+    answer: "Principio de la incertidumbre",
+    options: [
+      "Principio de energía mínima",
+      "Relaciones recíprocas de Onsager",
+      "Principio de Bernoulli",
+      "Principio de la incertidumbre"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Según la teória de la relatividad general, la gravedad no es una...",
+    answer: "una fuerza",
+    options: [
+      "una fuerza",
+      "una partícula",
+      "una onda",
+      "una bácteria"
+    ]
+  },
+    {
+    numb: 4,
+    question: "¿Cuántas dimensiones se incluyen en la teória de cuerdas?",
+    answer: "diez dimensiones",
+    options: [
+      "cinco dimensiones",
+      "ocho dimensiones",
+      "cuatro dimensiones",
+      "diez dimensiones"
+    ]
+  },
+    {
+    numb: 5,
+    question: "¿Qué sucede cuando las cuerdas vibran",
+    answer: "Se forman distintos elementos",
+    options: [
+      "Se forman distintos elementos",
+      "Desaparecen",
+      "Se multiplican",
+      "Generan energía"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
